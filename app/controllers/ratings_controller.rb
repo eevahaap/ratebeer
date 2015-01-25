3c46bbbd3b16d325a025a7f680ce8efa37bf0030
eevahaap <eeva.haapakangas@helsinki.fi>
2015-01-25 21:52:51 +0200

    vk2 teht10-11

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -14,6 +14,12 @@ class RatingsController < ApplicationController
     redirect_to ratings_path
   end
 
+  def destroy
+    rating = Rating.find(params[:id])
+    rating.delete
+    redirect_to ratings_path
+
+  end
 
 end
 
