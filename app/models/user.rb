7cb84d49421e80d41205f13a3c670157adb5b140
eevahaap <eeva.haapakangas@helsinki.fi>
2015-01-31 21:10:02 +0200

    vk3 teht5

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
 
   include RatingAverage
 
+
   validates :username, uniqueness: true,
-            length: { minimum: 3 }
+            length: { minimum: 3 , maximum: 15}
 
   has_many :ratings
 
