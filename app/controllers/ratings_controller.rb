2f77b9f2c7493b981019ddc77d5429e9c94bf4e1
eevahaap <eeva.haapakangas@helsinki.fi>
2015-02-03 22:04:32 +0200

    vk3

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -23,8 +23,8 @@ class RatingsController < ApplicationController
 
 
   def destroy
-    rating = Rating.find(params[:id])
-    rating.delete
+    rating = Rating.find params[:id]
+    rating.delete if current_user == rating.user
     redirect_to :back
   end
 
