46202781ad9d20e5da63d73480326099b51519e0
eevahaap <eeva.haapakangas@helsinki.fi>
2015-02-07 19:23:20 +0200

    vk3

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,11 @@ class User < ActiveRecord::Base
   validates :username, uniqueness: true,
             length: {in: 3..15}
   validates :password, format: { with: /(?=.*\d)(?=.*[A-Z])/, message: "must have at least one capital and one number" }
+  validates_length_of :password, minimum: 4
 
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :beer_clubs, through: :memberships
 
 end
