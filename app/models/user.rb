6b6f4e788e01368c0b9d3909eac9a14b5fd9d165
eevahaap <eeva.haapakangas@helsinki.fi>
2015-02-08 13:45:10 +0200

    vk4 teht3

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,29 @@ class User < ActiveRecord::Base
     ratings.order(score: :desc).limit(1).first.beer
   end
 
+  def favorite_style
+    favorite :style
+  end
+
+
+  private
+
+  def favorite(category)
+    return nil if ratings.empty?
+    rating_pairs = rated(category).inject([]) do |pairs, item|
+      pairs << [item, rating_average(category, item)]
+    end
+    rating_pairs.sort_by{ |s| s.last}.last.first
+  end
+
+  def rated(category)
+    ratings.map{ |r| r.beer.send(category)}.uniq
+  end
+
+  def rating_average(category, item)
+    ratings_of_item = ratings.select{ |r| r.beer.send(category) == item}
+    return 0 if ratings_of_item.empty?
+    ratings_of_item.inject(0.0){ |sum ,r| sum+r.score } / ratings_of_item.count
+  end
+
 end
