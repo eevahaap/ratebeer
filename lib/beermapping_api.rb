cfd569026fbbedc8f52c8272b9ff93dd7aa4aad4
eevahaap <eeva.haapakangas@helsinki.fi>
2015-02-13 20:26:43 +0200

    vk5 teht4

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,17 +1,24 @@
 class BeermappingApi
-  def self.places_in(city)
-    url = "http://beermapping.com/webservice/loccity/#{key}/"
-
-    response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
-    places = response.parsed_response["bmp_locations"]["location"]
-
-    return [] if places.is_a?(Hash) and places['id'].nil?
-
-    places = [places] if places.is_a?(Hash)
-    places.inject([]) do | set, place |
-      set << Place.new(place)
+    def self.places_in(city)
+      city = city.downcase
+      Rails.cache.fetch(city) { fetch_places_in(city) }
     end
-  end
+
+    private
+
+    def self.fetch_places_in(city)
+      url = "http://beermapping.com/webservice/loccity/#{key}/"
+
+      response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+      places = response.parsed_response["bmp_locations"]["location"]
+
+      return [] if places.is_a?(Hash) and places['id'].nil?
+
+      places = [places] if places.is_a?(Hash)
+      places.inject([]) do | set, place |
+        set << Place.new(place)
+      end
+    end
 
   def self.key
     "c34c664bd9eea6c8c3b6c089d7efc0c2"
